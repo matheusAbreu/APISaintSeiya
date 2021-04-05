86ce41968be043c60ea79df2f6ad9e437061bd12
Matheus Abreu <matheus.moon@gmail.com>
2021-04-05 03:28:08 -0300

    Corrigindo requisição de insert de tipos de guerreiros

--- src/controllers/WarriorTypesController.js
+++ src/controllers/WarriorTypesController.js
@@ -1,4 +1,5 @@
 const WarriorTypes = require('../models/WarriorTypes');
+const Gods = require('../models/Gods');
 
 module.exports = {
   async index(req, res) {
@@ -8,17 +9,18 @@ module.exports = {
   },
 
     async store(req, res) {
-        const { god_id } = req.params;
+        const { god } = req.params;
         const { name } = req.body;
 
-        const god = await User.findByPk(god_id);
-        console.log({god})
-        if (!god)
+        console.log({god })
+        const godFind = await Gods.findByPk(god);
+        console.log({godFind})
+        if (!godFind)
           return res.status(404).json({ error: 'god not found' });
 
       const newType = await WarriorTypes.create({
-        god_id,
-        name
+        god_id: god,
+        class_name: name
       });
 
       return res.json(newType);
