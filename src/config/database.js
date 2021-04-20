5d673086545ea5e9abadaf3a2b139d11e8d02b06
Matheus Abreu <matheus.moon@gmail.com>
2021-04-20 19:33:16 -0300

    add leitura de variavel de ambiente

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,22 @@
+const dotenv = require('dotenv');
+dotenv.config({ silent: true })
+
+const { 
+  DB_DATABASE,
+  DB_HOST,
+  DB_PASSWORD,
+  DB_PORT,
+  DB_USER,
+  DB_DIALECT,
+} = process.env;
+
 module.exports = {
-  dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'postgres',
-  database: 'api_saint_seiya',
+  dialect: DB_DIALECT,
+  host: DB_HOST,
+  username: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
+  port: DB_PORT,
   define: {
     timestamps: true,
     underscored: true,
